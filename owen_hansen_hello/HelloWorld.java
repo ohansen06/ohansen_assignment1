22553170ec4659b3aabbb76e8cb655852cef8529
ohansen06 <ohansen1@murraystate.edu>
2025-08-25 11:01:08 -0500

    commitlvl2

---
+++ owen_hansen_hello/HelloWorld.java
@@ -0,0 +1,35 @@
+package owen_hansen_hello;
+public class HelloWorld
+{
+    String name;
+    private int age;
+    public HelloWorld(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public HelloWorld(String name)
+    {
+        this.name = name;
+    }
+    public void greet()
+    {
+        System.out.println("Hello, " + name + "!");
+    }
+    public void introduce()
+    {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+    public void greet(String message)
+    {
+        System.out.println(message);
+    }
+    public static void main(String[] args)
+    {
+        HelloWorld student = new HelloWorld("Owen");
+        student.greet();
+        student.greet("Hey there!");
+        HelloWorld student2 = new HelloWorld("Owen", 19);
+        student2.introduce();
+    }
+}
