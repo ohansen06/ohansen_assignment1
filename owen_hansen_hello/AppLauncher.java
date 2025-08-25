c327b82366716d0a98db25e8e7a2c84ecf09bdbd
ohansen06 <ohansen1@murraystate.edu>
2025-08-25 12:40:13 -0500

    commit4(pending final)

--- owen_hansen_hello/AppLauncher.java
+++ owen_hansen_hello/AppLauncher.java
@@ -1,8 +1,9 @@
 package owen_hansen_hello;
 import java.util.Scanner;
+
 public class AppLauncher
 {
-    
+
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
@@ -10,8 +11,16 @@ public class AppLauncher
         int a;
         System.out.println("What is your name?");
         n = scan.nextLine();
+        if (n == "")
+        {
+            throw new NullPointerException("Cannot use a blank name");
+        }
         System.out.println("How old are you?");
         a = scan.nextInt();
+        if (a < 0)
+        {
+            throw new IllegalArgumentException("Cannot use a negative age");
+        }
         HelloWorld student = new HelloWorld(n);
         student.greet();
         student.greet("Hey there!");
